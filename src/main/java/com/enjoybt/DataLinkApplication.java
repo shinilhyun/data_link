ad2ed065ae1e777a2397a88d92ff57b2e63f4b87
shin88 <shin88@ejbt.co.kr>
2019-02-07 20:17:53 +0900

    화면 구성 전 커밋

--- src/main/java/com/enjoybt/DataLinkApplication.java
+++ src/main/java/com/enjoybt/DataLinkApplication.java
@@ -2,14 +2,23 @@ package com.enjoybt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class DataLinkApplication {
+public class DataLinkApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DataLinkApplication.class, args);
     }
+
+
 }
 
